965823c8ae852d161759d5bd87cc38126e11a49f
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-02-28 18:15:49 +0000

    added exercise c

---
+++ 28_feb_hw/exercise_c.py
@@ -0,0 +1,24 @@
+united_kingdom = [
+    {
+        "name": "Scotland",
+        "population": 5295000,
+        "capital": "Edinburgh"
+    },
+    {
+        "name": "Wales",
+        "population": 3063000,
+        "capital": "Swansea"
+    },
+    {
+        "name": "England",
+        "population": 53010000,
+        "capital": "London"
+    }
+]
+
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+united_kingdom["Wales"]["capital"] = "Cardiff"
+print(united_kingdom["Wales"]["capital"])
+# 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
+# 3. Use a loop to print the names of all the countries in the UK.
+# 4. Use a loop to find the total population of the UK.
