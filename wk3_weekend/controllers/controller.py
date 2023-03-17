24f4ac7ce5fdcbff6b5d0b9e4a40af95320f49b9
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-03-17 11:47:18 +0000

    progress made

--- wk3_weekend/controllers/controller.py
+++ wk3_weekend/controllers/controller.py
@@ -1,3 +1,8 @@
 from flask import *
 from app import app
 from models.book_class import Book
+from models.book_object import *
+
+@app.route('/books')
+def index():
+    return render_template('index.html', title='Home', books=book_list)
---
+++ wk3_weekend/models/book_object.py
@@ -0,0 +1,10 @@
+from book_class import Book
+
+book1 = Book("Now and Then", "William Corlett", "Fiction - Historical")
+book2 = Book("Small Things Like These", "Claire Keegan", "Fiction - Historical")
+book3 = Book("Taste", "Stanley Tucci", "Non-Fiction - Memoir")
+book4 = Book("I'm a Fan", "Sheena Patel", "Fiction - Contemporary")
+book5 = Book("The Dry", "Jane Harper", "Fiction - Thriller")
+book6 = Book("The Devil You Know", "Dr Gwen Adshead", "Non-Fiction - Sociology")
+
+book_list = [book1, book2, book3, book4, book5, book6]
