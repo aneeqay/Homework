ee95b9f0d34dc706a9df7e4d2323c5ada902c4e6
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-04-03 19:19:51 +0100

    finished hw

--- 3_apr_hw/whodunnit.js
+++ 3_apr_hw/whodunnit.js
@@ -14,19 +14,173 @@
 // prediction: The murderer is Miss Scarlet.
 
 // Episode 2
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// prediction: The murderer is Professor Plum.
+// actual: TypeError: Assignment to constant variable.
+
+// Episode 3
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+// prediction: First Verdict: Mrs Peacock, Second Verdict: Professor Plum
+
+// Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+// prediction: The suspects are Miss Scarlet, Professor Plum, Colonel Mustard. Suspect three is Mrs. Peacock.
+
+// Episode 5
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+// prediction: The weapon is the Revolver.
+
+// Episode 6
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// prediction: The murderer is Mrs White.
+
+// Episode 7
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// prediction: The murderer is Colonel Mustard.
+// actual: The murderer is Mr Green.
+
+// Episode 8
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+
+//   plotTwist('Dining Room');
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+// prediction: The weapon is Lead Pipe.
+// actual: The weapon is Candle Stick.
+
+// Episode 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
 }
 
 const declareMurderer = function() {
   return `The murderer is ${murderer}.`;
 }
 
-changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
 // prediction: The murderer is Professor Plum.
-// actual: TypeError: Assignment to constant variable.
 
